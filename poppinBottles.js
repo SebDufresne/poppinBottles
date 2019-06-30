ed8bc35b5067b515b9db70729bebc2aa72f66e2c
Sebastien Dufresne <s.dufresne@dreaminggiraffe.ca>
2019-06-30 20:28:28 +0000

    Refactored calculateBottles() for a DRY approach. created addEmpty() and addRecycle()

--- poppinBottles.js
+++ poppinBottles.js
@@ -5,21 +5,22 @@ const getArguments = () => {
 // Pass an amount of money as int and returns an object
 const calculateBottles = (money,bottles,summary) => {
   summary = (summary || {
-    money : {total: 0, left: 0},
-    fullBottles : { bought: 0},
-    emptyBottles : { total: 0, left: 0, earned : 0 },
-    bottleCaps : { total: 0, left : 0, earned : 0 },
+    money : {left: 0},
+    total : {money: 0, bottle: 0, empty: 0, caps: 0}, // Refactored
+    bottles : { bought: 0},
+    empty : { left: 0, earned : 0 },
+    caps : { left : 0, earned : 0 },
   });
 
   let fullBottles = 0;
 
   // If the parameter as some money as input, add it
-  if (money) summary.money.total += money;
+  if (money) summary.total.money += money;
 
   // if there's enoug money to buy some bottles
   if ((money + summary.money.left) >= 2) {
     fullBottles += Math.floor(money / 2);
-    summary.fullBottles.bought += fullBottles;
+    summary.bottles.bought += fullBottles;
     money = money % 2;
   }
 
@@ -28,25 +29,25 @@ const calculateBottles = (money,bottles,summary) => {
 
   // Caculate emptyBottles / caps from previous bottles number
   if (bottles) {
-    summary.emptyBottles.total += bottles;
-    summary.bottleCaps.total += bottles;
-    summary.emptyBottles.left += bottles;
-    summary.bottleCaps.left += bottles;
+    summary.total.empty += bottles;
+    summary.total.caps += bottles;
+    summary.empty.left += bottles;
+    summary.caps.left += bottles;
   }
 
-  // if we have enough emptyBottles to get bottles
-  if (summary.emptyBottles.left >= 2) {
-    fullBottles += Math.floor(summary.emptyBottles.left / 2);
-    summary.emptyBottles.earned += Math.floor(summary.emptyBottles.left / 2);
-    summary.emptyBottles.left = summary.emptyBottles.left % 2;
-  }
+  const addEmpty = (qty, freeRatio) => Math.floor(qty / freeRatio);
 
-  // if we have enough bottleCaps to get bottles
-  if (summary.bottleCaps.left >= 4) {
-    fullBottles += Math.floor(summary.bottleCaps.left / 4);
-    summary.bottleCaps.earned += Math.floor(summary.bottleCaps.left / 4);
-    summary.bottleCaps.left = summary.bottleCaps.left % 4;
-  }
+  const addRecycle = (itemObj,freeRatio) => {
+    itemObj.earned += Math.floor(itemObj.left / freeRatio);
+    itemObj.left = itemObj.left % freeRatio;
+    return itemObj;
+  };
+
+  fullBottles += addEmpty(summary.empty.left, 2);
+  fullBottles += addEmpty(summary.caps.left, 4);
+
+  summary.empty = addRecycle(summary.empty,2);
+  summary.caps = addRecycle(summary.caps,4);
 
   if (fullBottles) {
     summary = calculateBottles(0,fullBottles,summary);
@@ -58,17 +59,17 @@ const calculateBottles = (money,bottles,summary) => {
 const printSummary = summary => {
   const log = console.log;
 
-  const earnedFromBottles = summary.emptyBottles.earned;
-  const earnedFromCaps = summary.bottleCaps.earned;
+  const earnedFromBottles = summary.empty.earned;
+  const earnedFromCaps = summary.caps.earned;
 
-  const remainingCaps = summary.bottleCaps.left;
-  const remainingBottles = summary.emptyBottles.left;
+  const remainingCaps = summary.caps.left;
+  const remainingBottles = summary.empty.left;
 
   const totalEarned = earnedFromBottles + earnedFromCaps;
-  const totalBottles = totalEarned + summary.fullBottles.bought;
+  const totalBottles = totalEarned + summary.bottles.bought;
 
-  const earnedBottles = summary.emptyBottles.earned;
-  const earnedCaps = summary.bottleCaps.earned;
+  const earnedBottles = summary.empty.earned;
+  const earnedCaps = summary.caps.earned;
 
   log(`TOTAL BOTTLES: ${totalBottles}`);
   log(`REMAINING BOTTLES: ${remainingBottles}`);
